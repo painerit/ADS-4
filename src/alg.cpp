7351b4bfbcc7ee6e8f67fe6579c1705909c82bd5
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-03-24 22:43:10 +0300

    Филимонов Павел 21-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,28 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int sum = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = (i + 1); j < len; j++) {
+      if (arr[i] + arr[j] == value)
+        sum++;
+    }
+  }
+  return sum;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int sum = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = (len - 1); j > i; j--) {
+      if ((j >= value) || (j < (value/2)))
+        continue;
+      else if (arr[i] + arr[j] == value)
+        sum++;
+    }
+  }
+  return sum;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int sum = 0;
+  for (int i = 0; i < len; i++) {
+  }
 }
