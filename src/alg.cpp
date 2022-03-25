7db27ca05f8db2abc66cae1f463a31b297b0500d
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-03-25 13:45:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,8 +21,38 @@ int countPairs2(int *arr, int len, int value) {
   }
   return sum;
 }
+int nums(int *arr, int len, int mid, int sum) {
+  sum++;
+  int mid2 = mid;
+  while ((arr[mid2] == arr[mid + 1]) && (arr[mid+1] < len)) {
+    sum++;
+    mid2++;
+  }
+  while ((arr[mid] == arr[mid - 1]) && (arr[mid-1] >= 0)) {
+    sum++;
+    mid--;
+  }
+  return sum;
+}
+int BinSearch (int *arr, int len, int value, int left, int sum) {
+  int right = len - 1, num = left;
+  while(left < right) {
+    int mid = left + (right - left) / 2;
+    if (num + arr[mid] == value)
+      return nums(arr, len, mid, sum);
+    else if (num + arr[mid] > value)
+      right = mid;
+    else
+      left = mid + 1;
+  }
+  return sum;
+}
 int countPairs3(int *arr, int len, int value) {
   int sum = 0;
   for (int i = 0; i < len; i++) {
+    if (value/2 <= arr[i])
+      break;
+   sum = BinSearch(arr, len, value, i, sum);
   }
+  return sum;
 }
